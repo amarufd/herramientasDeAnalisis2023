04e4e348d702f62d3fe3b7d60e6f603066f85ee1
Amaru Fernandez <amarud@gmail.com>
2023-05-22 19:57:41 -0400

    mas info

---
+++ Python/bash_python.py
@@ -0,0 +1,12 @@
+# Esto es un comentario
+
+'''
+Desarrollador: Amaru Fernandez
+Ubicacion: santiago
+Version python: 3.8
+'''
+
+nombre = "Amaru"
+apellido = "Fernandez"
+nombre_completo = nombre + " " + apellido
+print(nombre_completo)
